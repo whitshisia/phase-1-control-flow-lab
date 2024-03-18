0247fae29cfcc0a5852d011ce248ac87b7f61697
whitney shisia <shisiawhitney215@gmail.com>
2024-03-18 19:39:19 +0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return `This one is on me!`;
+  } else if (ride > 2500) {
+    return 'No can do.';
+} else if (ride > 2000) {
+    return 'I will gladly take your thirty bucks.';
+}
+return 'That will be twenty bucks.';
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+    switch (tip) {
+      case 'generous':
+          return 'Thank you so much.';
+      case 'not as generous':
+          return 'Thank you.';
+      default:
+          return 'Bye.';
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  
